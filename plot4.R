c05ad2f07c65c26b72fe1c3f8eecdd26bab301fe
Michael Yap <miyap.2@gmail.com>
2016-04-09 13:34:54 +0800

    Exploratary Analysis Wk1 Assignment

--- plot4.R
+++ plot4.R
@@ -54,7 +54,7 @@ with(hpcdata, { ##Upper-Left
                        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
                        col=c("black", "red", "blue"), 
                        lty=1, 
-                       lwd=1) 
+                       lwd=1, bty="n") 
                 ##Lower Right 
                 plot(Global_reactive_power ~ datetime, 
                      type="l", 
